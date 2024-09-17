906c4091474f03c8b0a4629f5b51332d111be480
José Maria Macedo <83874640+JoseMariaMacedo@users.noreply.github.com>
2024-09-17 17:31:00 +0000

    testando mais comandos

--- meu-projeto-java/src/com/primeiro_projeto/PrimeiroProgramaExemplo.java
+++ meu-projeto-java/src/com/primeiro_projeto/PrimeiroProgramaExemplo.java
@@ -1,7 +1,14 @@
 package com.primeiro_projeto;
 
+import java.util.Scanner;
+
 public class PrimeiroProgramaExemplo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
         System.out.println("Olá, mundo! Meu primeiro código em Java");
+        System.out.println(nome);
+        scanner.close();
     }
 }
