5c2560046fc59e821e4225b57c367cb102519cff
José Maria Macedo <83874640+JoseMariaMacedo@users.noreply.github.com>
2024-09-17 00:42:49 +0000

    meu primeiro código

---
+++ meu-projeto-java/src/com/primeiro_projeto/PrimeiroProgramaExemplo.java
@@ -0,0 +1,7 @@
+package com.primeiro_projeto;
+
+public class PrimeiroProgramaExemplo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Meu primeiro código em Java");
+    }
+}
